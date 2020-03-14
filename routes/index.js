61fe441fc18acb94999ba012374f56cd30312ac7
zakipuzo <aarabzakaria7@gmail.com>
2020-03-14 13:07:42 +0000

    first commit

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+var request = require("request");
+
+function getmap(callback) {
+
+var options = {
+  method: 'GET',
+  url: 'https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php',
+  headers: {
+    'x-rapidapi-host': 'coronavirus-monitor.p.rapidapi.com',
+    'x-rapidapi-key': 'fdc23100b4msh680bf233168b249p111996jsn9cf1a0740f23'
+  }
+};
+
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	//console.log(body);
+
+	return callback(body);
+});
+}
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+	//console.log(req.headers.host);
+	
+		 res.render('index', { title: "CORONAVIRUS"});
+	
+ 
+});
+
+
+router.get('/zakipuzo', function(req, res, next) {
+getmap(function(body){
+res.send(body);
+});
+});
+
+
+
+module.exports = router;
